6f722ae7c5f862a834838adffd711ab1f628a7a7
OnurKlc <onurkilic5@gmail.com>
2020-08-12 00:37:20 +0300

    escape cdn cache for rss feed service

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -31,8 +31,10 @@ export class BlogComponent implements OnInit {
   };
 
   ngOnInit(): void {
+    const now = new Date();
+    const nowParam = now.getTime();
     // tslint:disable-next-line:max-line-length
-    this.http.get<any>('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@joatmonnn').subscribe(data => {
+    this.http.get<any>('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@joatmonnn?t=' + nowParam).subscribe(data => {
       for (const item of data.items) {
         // tslint:disable-next-line:max-line-length
         item.description = item.description.substring(item.description.indexOf('</figure><p>') + 12, item.description.indexOf('</figure><p>') + 125) + '...';
