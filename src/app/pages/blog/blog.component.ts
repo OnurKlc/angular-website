22be7c1306963d9f2f2ad8a7f90bb6b8e154a7d2
OnurKlc <onurkilic5@gmail.com>
2020-08-15 00:09:40 +0300

    rss2json api cache workaround

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -31,10 +31,9 @@ export class BlogComponent implements OnInit {
   };
 
   ngOnInit(): void {
-    const now = new Date();
-    const nowParam = now.getTime();
+    const randomText = Math.random().toString(16).slice(2);
     // tslint:disable-next-line:max-line-length
-    this.http.get<any>('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@joatmonnn?t=' + nowParam).subscribe(data => {
+    this.http.get<any>('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@joatmonnn?t=' + randomText).subscribe(data => {
       for (const item of data.items) {
         // tslint:disable-next-line:max-line-length
         item.description = item.description.substring(item.description.indexOf('</figure><p>') + 12, item.description.indexOf('</figure><p>') + 125) + '...';
